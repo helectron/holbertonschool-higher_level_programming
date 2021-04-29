f264493b79d419b0994a01bb0978ec77f0e285d4
helectron <helena.cortes@udea.edu.co>
2021-04-29 22:03:11 +0000

    Return in the wrong place and betty style for better understanding of the code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,22 @@
 #include "lists.h"
 /**
- * check_cycle - check if there is a cycle in the linked list 
- * @list: pointer to the head of the list
- * @One: go throught the linked list one by one
- * @Two: go throught the linled list two by two
- * Return: 0 if there is a cycle, 1 if there is not
+ *check_cycle - check if there is a cycle in the linked list
+ *@list: pointer to the head of the list
+ *Return: 0 if there is a cycle, 1 if there is not
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *One = list;
-    listint_t *Two = list;
+listint_t *One = list;
+listint_t *Two = list;
 
-    while (One && Two && Two->next)
-    {
-	One = One->next;
-	Two = Two->next->next;
-	if (One == Two)
-	{
-	    return (1);
-	}
-	return (0);
-    }
+while (One && Two && Two->next)
+{
+One = One->next;
+Two = Two->next->next;
+if (One == Two)
+{
+return (1);
+}
+}
+return (0)
 }
