40b715b5ca0337e8d2d7f161e5628ef819f4d0f0
helectron <helena.cortes@udea.edu.co>
2021-05-17 02:10:26 +0000

    Exceptions project

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        while counter < x:
+            print("{}".format(my_list[counter]), end='')
+            counter += 1
+
+    except IndexError:
+        print()
+        return(counter)
+    print()
+    return(counter)
