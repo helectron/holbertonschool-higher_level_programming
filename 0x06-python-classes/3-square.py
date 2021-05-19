acaa5daa49df222b5d33c303205f3e572308eec1
helectron <helena.cortes@udea.edu.co>
2021-05-18 22:48:37 -0500

    public instance to return square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' This module contains one class: Square '''
+
+
+class Square:
+    ''' This class defines a square '''
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return(self.__size * self.__size)
