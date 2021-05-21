723479ec983f5322112b334e878355852d1fc6e5
helectron <helena.cortes@udea.edu.co>
2021-05-20 23:13:54 -0500

    changing a conditional

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,13 +18,13 @@ class Rectangle:
     @property
     def width(self):
         '''gets, retrieves width'''
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
         '''set the value of width'''
         self.__width = value
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -32,13 +32,13 @@ class Rectangle:
     @property
     def height(self):
         '''set the value of height'''
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
         '''set the value of height'''
         self.__height = value
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
