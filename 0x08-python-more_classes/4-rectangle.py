e2c93bb97f9be6f2348c646c29831bf87c930cb0
helectron <helena.cortes@udea.edu.co>
2021-05-20 23:56:09 -0500

    exess of code

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -13,19 +13,8 @@ class Rectangle:
         of an object based on a
         rectangle class
         '''
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
