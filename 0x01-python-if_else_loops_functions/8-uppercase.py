92a6340253f0c429108e79ff96040b16847a7d64
helectron <helena.cortes@udea.edu.co>
2021-05-23 16:46:26 -0500

    correcting asci and printing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 65 and ord(i) <= 90:
-            i = chr(ord(i) + 32)
-    print(str)
-
+        if ord(i) >= 97 and ord(i) <= 123:
+            i = chr(ord(i) - 32)
+    print("{}".format(i), end="")
+    print("")
