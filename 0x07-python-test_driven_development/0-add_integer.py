709f28c43a7af5895d1fa61c8bb93704478369ca
helectron <helena.cortes@udea.edu.co>
2021-05-24 21:18:23 -0500

    functions comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/user/bin/python3
+'''
+This module is a task for the project Python Test Driven Development
+for Holberton School that supplies a function to add integers.
+
+To test run: python -m doctest -v ./tests/0-add_integer.txt
+'''
 def add_integer(a, b=98):
+'''A function to calculate the sum of two integers'''
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
