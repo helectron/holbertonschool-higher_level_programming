ffc55f17b51d79dd537975d1cedfc42f1085a9a3
helectron <helena.cortes@udea.edu.co>
2021-05-25 18:12:39 -0500

    raising errors

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,4 +7,9 @@ Test it running: python3 -m doctest -v ./tests/3-say_my_name.txt
 
 def say_my_name(first_name, last_name=""):
     '''The function prints a string and two arguments'''
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {:s} {:s}".format(first_name, last_name))
