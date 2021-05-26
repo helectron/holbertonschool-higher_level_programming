00ccd2877e21debe669f7596a6175e4c1dab2a0d
helectron <helena.cortes@udea.edu.co>
2021-05-26 12:05:52 -0500

    text identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+A module that supplues one function: text_identation().
+
+To test, run: `python3 -m doctest ./tests/5-text_indentation.txt`
+'''
+
+
+def text_indentation(text):
+    ''' 
+    prints to stdout text with 2 new lines after certain characters
+    Arguments:
+    text: must be a string
+    '''
+    if type(text) is not str or len(text) < 0:
+        raise TypeError("text must be a string")
+    # replace chars with placeholder
+    text = text.replace('.', '.\\')
+    # print(text)
+    text = text.replace('?', '?\\')
+    text = text.replace(':', ':\\')
+    print(text, end='')
