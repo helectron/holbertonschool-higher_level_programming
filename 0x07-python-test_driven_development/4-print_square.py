8cbbdf798600bdc630d2f9603afa76dc923065d9
helectron <helena.cortes@udea.edu.co>
2021-05-25 22:20:36 -0500

    A module that supplies one function that prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''A module that supplies one function that prints a square'''
+
+
+def print_square(size):
+    '''The function prints a square with the # char'''
+
+    if not isinstance(size, int) or isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for count in range(size):
+        print('#' * size)
