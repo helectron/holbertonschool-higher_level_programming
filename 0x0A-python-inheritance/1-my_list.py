6a5b5be666a6c088168798d34af2371a66dda26a
helectron <helena.cortes@udea.edu.co>
2021-06-01 15:42:22 -0500

    Class for printing a list in ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module 1-my_list
+Class:
+    MyList (list)
+'''
+
+
+class MyList(list):
+    '''Class for printing a list in ascending order'''
+
+def print_sorted(self):
+    '''Prints self list in ascending order'''
+    print(sorted(list))
