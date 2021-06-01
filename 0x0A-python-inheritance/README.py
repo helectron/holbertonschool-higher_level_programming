889490edc539a8eaf92b82f042cd063769a99536
helectron <helena.cortes@udea.edu.co>
2021-06-01 14:33:54 -0500

    fixing the image

--- 0x0A-python-inheritance/README.py
+++ 0x0A-python-inheritance/README.py
@@ -1,6 +1,7 @@
 # Python - Inheritance
 
-![Python inheritance](https://miro.medium.com/max/736/1*FTbU-SUXe9Cot7gX1F2bNQ.jpeg)
+![Image](https://miro.medium.com/max/736/1*FTbU-SUXe9Cot7gX1F2bNQ.jpeg)
+
 
 :+1: :sparkles: :camel: :tada:
 :rocket: :metal: :octocat:
