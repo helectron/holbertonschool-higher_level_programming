8636a73ab29b56770a176957a262481bb48fc23e
helectron <helena.cortes@udea.edu.co>
2021-06-01 12:27:52 -0500

    Readme and a module for getting a list of attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for getting list of attributes and methods'''
+
+
+def lookup(obj):
+    '''Returns a list of attributes and methods of an object parameter'''
+    return dir(object)
---
+++ 0x0A-python-inheritance/README.py
@@ -0,0 +1,6 @@
+# Python - Inheritance
+
+![Python inheritance](https://miro.medium.com/max/736/1*FTbU-SUXe9Cot7gX1F2bNQ.jpeg)
+
+:+1: :sparkles: :camel: :tada:
+:rocket: :metal: :octocat:
