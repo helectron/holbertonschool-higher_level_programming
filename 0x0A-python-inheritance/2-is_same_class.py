066e2e9806259bb9a76d775ace0ee96c23e03d8f
helectron <helena.cortes@udea.edu.co>
2021-06-01 15:59:36 -0500

    Check if the object is an instance or not

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module 3-is_kind_of_class
+Functions:
+    is_kind_of_class(obj, a_class)
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Check if the object is an instance of, or if the object
+       is an instance of a class that inherited from, the specified class
+
+    Arguments:
+        obj: the object to evaluate
+        a_class: the class to compare the object
+
+    Return:
+        True if the object is an instance of, or if the object is an instance
+        of a class that inherited from, the specified class. Otherwise False
+    '''
+    return isinstance(obj, a_class)
