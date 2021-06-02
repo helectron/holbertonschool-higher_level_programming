7162b23c76315a7326c166ddbd56f093748d9ebb
helectron <helena.cortes@udea.edu.co>
2021-06-01 23:36:34 -0500

    creates a rectangle object

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module 8-rectangle
+Classes
+    Rectangle
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Creates a Rectangle object
+    Methods
+        __init__(self, width, height)
+    '''
+
+    def __init__(self, width, height):
+        '''Creates object
+        Arguments
+            width: width value integer
+            height: height value integer
+        '''
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
