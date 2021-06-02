6443c2c2917d4f90048390d6bac1f6fa21ed7482
helectron <helena.cortes@udea.edu.co>
2021-06-01 20:44:37 -0500

    raising an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module 5-base_geometry
+Classes
+    BaseGeometry
+'''
+
+
+class BaseGeometry():
+    '''Empty BaseGeometry class object'''
+    pass
+
+    def area(self):
+        '''raises an exception'''
+        raise Exception("area() is not implemented")
