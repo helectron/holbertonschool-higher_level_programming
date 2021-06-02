d4dc9fc104e9554c39825f384e3c694cafe23b12
helectron <helena.cortes@udea.edu.co>
2021-06-01 20:19:35 -0500

    a better documentation for lookup and of course the Class MyList

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-'''Module for getting list of attributes and methods'''
+'''Module 0-lookup
+Functions:
+    lookup(obj)
+'''
 
 
 def lookup(obj):
-    '''Returns a list of attributes and methods of an object parameter'''
+    '''Returns the list of available attributes and methods of an object
+    Args:
+            obj (obj): object to get attributes from
+    Return:
+            a list object with all the attributes and methods
+    '''
     return dir(object)
