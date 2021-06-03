4b00fe049e2582423d4443d0e8d282aa93025a08
helectron <helena.cortes@udea.edu.co>
2021-06-02 22:24:28 -0500

    reading a stdout text from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''module 0-read_file
+Functions:
+    read_file(filename="")
+'''
+
+def read_file(filename=""):
+    '''read to stdout text from a file
+    Arguments:
+        filename: string with file's path
+    '''
+    with open(filename, mode="r", encoding="utf-8") as myFile:
+        print(myFile.read(), end="")
+    myFile.close()
