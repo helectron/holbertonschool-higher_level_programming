b2b789c7fcccedf9ff820e469924930d91073122
helectron <helena.cortes@udea.edu.co>
2021-06-07 19:24:44 -0500

    fixing the init

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ from models.base import Base
 class Rectangle(Base):
     ''' constructor '''
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().init__(id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
