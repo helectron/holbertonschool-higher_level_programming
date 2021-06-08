d40d608fa7d7a3fbdcefa1d7490cf0b63cc1199c
helectron <helena.cortes@udea.edu.co>
2021-06-08 17:02:33 -0500

    proper docstrings

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 '''
-Contain a super class
-call Base
+module base
+Class
+    Base
 '''
 
 
 class Base:
-    ''' The first class is Base'''
+    ''' 
+    Manage 'id' attribute in all furture instances of this class
+    and to avoid duplicating the same code (by extension, same bugs)
+    Class Attributes:
+        __nb_objects (int): number of instances from Base class
+    Methods:
+        __init__(self, id=None)
+        '''
 
     __nb_objects = 0  # Class attribute
 
     def __init__(self, id=None):  # Initializer for instantiate Base object
         '''
         Constructor
-        Initializar for Base class
+        if 'id' is set to 'None' the '__nb_objects' is 
+        incremented by one and set as the id for the new object
+        Arguments:
+            id (int): id number for the new object
         '''
 
         if id is not None:  # If the user pass id, shall be assign
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
 '''
-Subclass rectangle inherits
-from Base class
+module rectangle
+Class
+    Rectangle
 '''
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    ''' constructor '''
+    '''
+    Creates a Rectangle object
+    Properties:
+        width (int): width of the instance
+        height (int): height of the instance
+        x (int): x coordenate
+        y (int): y coordenate
+        id (int): object identificator
+    Methods:
+        __init__(self, width, height, x=0, y=0, id=None)
+        def area(self)
+        display(self)
+    Magic Methods:
+        __str__(self)
+    '''
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''
+        Constructor
+        Id parameter is set by the superclass `Base`
+        Arguments:
+            width (int): width of the instance
+            height (int): height of the instance
+            x (int): x coordenate
+            y (int): y coordenate
+            id (int): object identificator
+        '''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -23,6 +48,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        '''sets width'''
         self.__width = value
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -31,11 +57,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        '''Retrieve the with of Rectangle'''
+        '''Retrieve the height of Rectangle'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''Retrieve the height of Rectangle'''
         self.__height = value
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -44,11 +71,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        '''Retrieve the with of Rectangle'''
+        '''Return x'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''Sets x'''
         self.__x = value
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
@@ -57,40 +85,57 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        '''Retrieve the with of Rectangle'''
+        '''Return y'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''sets y'''
         self.__y = value
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
 
+    # Methods
     def area(self):
         '''returns the area of a rectangle'''
         return self.height * self.width
 
     def display(self):
         '''
-        Prints in stdout the Rectangle instance
-        with the character #
-        x: spaces at the beggining of the line
-        y: new line
+        print to stdout the graphic representation
+        of the object taking x and y as reference
+        to the position of the object in the screen
         '''
         print("\n" * self.__y, end='')
         for iterator in range(self.__height):
             print(" " * self.__x, end='')
             print("#" * self.__width)
 
+    # Magic method
     def __str__(self):
+        '''
+        overriding the __str__ method so that it returns
+        `[Rectangle] (<id>) <x>/<y> - <width>/<height>
+        '''
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(__class__.__name__,
                                                         self.id, self.x,
                                                         self.y, self.width,
                                                         self.height))
 
     def update(self, *args):
+        '''
+        Update the object's attributes
+        Arguments
+            args (int): packed list, the order of arguments
+            must be as follow:
+            - 1st argument should be the id attribute
+            - 2nd argument should be the width attribute
+            - 3rd argument should be the height attribute
+            - 4th argument should be the x attribute
+            - 5th argument should be the y attribute
+        '''
         attributes = ['id', 'width', 'height', 'x', 'y']
         if args and args[0]:
             for i in range(len(args)):
