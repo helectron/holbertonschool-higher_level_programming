36121fe5e29b4dba01d9d405ff4a6232359967e6
helectron <helena.cortes@udea.edu.co>
2021-06-07 19:20:02 -0500

    fixing getter and setter values

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     ''' constructor '''
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -23,24 +23,24 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.size = value
+        self.__width = value
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
 
     @property
     def height(self):
         '''Retrieve the with of Rectangle'''
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
-        self.size = value
+        self.__height = value
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
 
     @property
     def x(self):
@@ -49,21 +49,22 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.size = value
+        self.__x = value
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("x must be >= 0")
 
     @property
     def y(self):
         '''Retrieve the with of Rectangle'''
-        return self.__height
+        return self.__y
 
     @y.setter
-    def height(self, value):
-        self.size = value
+    def y(self, value):
+        self.__y = value
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("y must be >= 0")
+
