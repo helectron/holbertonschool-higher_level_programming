c2382f0f1bad20e63a72c2f9454284f69f13ab41
helectron <helena.cortes@udea.edu.co>
2021-06-08 16:38:53 -0500

    documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-''' 0-main '''
+'''
+Contain a super class
+call Base
+'''
 
 
 class Base:
+    ''' The first class is Base'''
 
     __nb_objects = 0  # Class attribute
 
     def __init__(self, id=None):  # Initializer for instantiate Base object
+        '''
+        Constructor
+        Initializar for Base class
+        '''
+
         if id is not None:  # If the user pass id, shall be assign
-            self.id = id  # Unique instance attribute, assign id with this argument value
+            self.id = id  # Unique instance attr, assign id with this arg value
         else:  # If there is not id, assign the counter one
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
